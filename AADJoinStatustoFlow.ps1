08bed6d9b5f3f07cc9c722f8399fea8c86809124
David Just <57944742+djust270@users.noreply.github.com>
2021-10-07 05:53:34 -0400

    Add files via upload

---
+++ AADJoinStatustoFlow.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.Synopsis
+    Sample code, grabbing data from a computer (in this case AD/AAD join status) and sending it to a PowerAutomate flow using the HTTP Request flow trigger. 
+#>
+$joinstatus = dsregcmd /status | Select-String -SimpleMatch 'AzureAdJoined'
+$domainstatus = dsregcmd /status | Select-String -SimpleMatch 'DomainJoined'
+
+if ($joinstatus -like '*yes*'){$aadstatus = "IsJoined"}else{$aadstatus = "NOTJoined"}
+if ($domainstatus -like '*yes*'){$DomainJoinStatus = "IsJoined"}else{$DomainJoinStatus = "NOTJoined"}
+$CompInfo = [System.Collections.Generic.List[PSObject]]::new()
+$CompInfo.add([pscustomobject]@{
+    key = "$env:ComputerName"
+    ComputerName = "$env:ComputerName"
+    User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name 
+    AADJoinStatus = $aadstatus
+    DomainJoinStatus = $DomainJoinStatus
+    }
+   )
+
+
+$header = @{ 'Content-Type' = "application/json" }
+$flow = # URL in PowerAutomate Flow for HTTP Request
+$flowheader = $compinfo | ConvertTo-Json -Compress
+Invoke-RestMethod -Method Post -Body $flowheader -uri $flow -Headers $header
+		
---
+++ Get-UninstallKeys.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.Synopsis 
+    Return uninstall registry key properties for all installed software, including software installed in user context. Can be run as system. 
+
+.Outputs 
+    Selected.System.Management.Automation.PSCustomObject
+
+.Example
+    
+    PS> Get-UninstallKeys | where displayname -eq "Zoom" | Select displayname,displayversion,uninstallstring,quietuninstallstring 
+    
+    ---------------------------Description--------------------------
+    Filter for specific app, select desired properties
+    
+.Example
+
+    PS> Start-Process  ((Get-UninstallKeys | where displayname -eq "Zoom").UninstallString).split()[0] -argumentlist ((Get-UninstallKeys | where displayname -eq "Zoom").UninstallString).split()[1]
+
+    ---------------------------Description--------------------------
+    Uninstall the selected app using start process. 
+
+.Notes 
+    Created by : David Just
+    Date coded : 10/07/2021
+    Repository : https://github.com/djust270
+#>
+function Get-UninstallKeys
+{
+$UserSID = (New-Object -ComObject Microsoft.DiskQuota).TranslateLogonNameToSID((Get-CimInstance -Class Win32_ComputerSystem).Username)
+$uninst = "registry::HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall","registry::HKLM\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall","registry::HKU\$UserSID\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+$uninst | foreach {$u = $_
+$table = Get-Childitem $u | Get-ItemProperty | where displayname -ne $null | select displayname,DisplayVersion,UninstallString,QuietUninstallString,InstallLocation | Sort-Object -Property displayname 
+$table }
+
+    Return $table
+}
