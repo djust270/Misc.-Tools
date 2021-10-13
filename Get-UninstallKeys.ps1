608b35ba15d268f2a6dd78973d28b1af5f590a95
David Just <57944742+djust270@users.noreply.github.com>
2021-10-12 20:53:32 -0400

    Update Get-UninstallKeys.ps1

--- Get-UninstallKeys.ps1
+++ Get-UninstallKeys.ps1
@@ -27,10 +27,10 @@
 function Get-UninstallKeys
 {
 $UserSID = (New-Object -ComObject Microsoft.DiskQuota).TranslateLogonNameToSID((Get-CimInstance -Class Win32_ComputerSystem).Username)
+$table = [System.Collections.Generic.List[psobject]]::new()
 $uninst = "registry::HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall","registry::HKLM\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall","registry::HKU\$UserSID\Software\Microsoft\Windows\CurrentVersion\Uninstall"
 $uninst | foreach {$u = $_
-$table = Get-Childitem $u | Get-ItemProperty | where displayname -ne $null | select displayname,DisplayVersion,UninstallString,QuietUninstallString,InstallLocation | Sort-Object -Property displayname 
-$table }
+$table.add((Get-Childitem $u | Get-ItemProperty | where displayname -ne $null | select displayname,DisplayVersion,UninstallString,QuietUninstallString,InstallLocation | Sort-Object -Property displayname)) }
 
     Return $table
 }
