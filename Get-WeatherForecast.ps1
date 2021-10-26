cde22fbd7f5a19b13b5ab19d13b9adce6d5317bc
David Just <57944742+djust270@users.noreply.github.com>
2021-10-26 15:54:10 -0400

    Add files via upload

---
+++ Get-WeatherForecast.ps1
@@ -0,0 +1,58 @@
+function Get-WeatherForecast {
+param (
+[parameter(mandatory=$true)]
+[string]$zipcode
+)
+
+#region Weather Variables
+
+#Get Lattitude and Longitude for your zip code
+$getLatLong = Invoke-restmethod -uri "https://api.promaptools.com/service/us/zip-lat-lng/get/?zip=$zipcode&key=17o8dysaCDrgv1c" | select -ExpandProperty output
+
+#Get weather forecast endpoint for your zipcode
+$getPoints = Invoke-RestMethod -uri "https://api.weather.gov/points/$($getLatLong.latitude),$($getLatLong.longitude)"
+
+#Get 7 day weather forecast
+$weatherGet = Invoke-RestMethod -Uri $getPoints.properties.forecast
+
+#Get todays date. Manipulate string to match JSON date object returned from api.weather.gov
+$TodayManipulate = ((get-date).ToShortDateString() -replace '/','-') -split '-'
+$today = "$($TodayManipulate[2] + '-' + $TodayManipulate[0] + '-' + $TodayManipulate[1])*"
+
+# Do the same for tomorrows date
+$TomorrowManipulate = (((get-date).AddDays(1)).ToShortDateString() -replace '/','-') -split '-'
+$tomorrow = "$($TomorrowManipulate[2] + '-' + $TomorrowManipulate[0] + '-' + $TomorrowManipulate[1])*"
+
+# Get today and tomorrows forecasted temperatures
+$todaysTemp = Invoke-RestMethod -Uri 'https://api.weather.gov/gridpoints/LWX/102,88' | select -ExpandProperty properties | select -ExpandProperty temperature | select -ExpandProperty values | select -ExcludeProperty values | where validtime -like "$($today)" | sort value
+$tomorrowsTemp = Invoke-RestMethod -Uri 'https://api.weather.gov/gridpoints/LWX/102,88' | select -ExpandProperty properties | select -ExpandProperty temperature | select -ExpandProperty values | select -ExcludeProperty values | where validtime -like "$($tomorrow)" | sort value
+
+# Convert temps to farenheit
+$TodaysTempF = $todaysTemp | select @{n="Temp";e={$_.value | foreach {[int]($_ * 1.8) + 32}}}
+$tomorrowsTempF = $tomorrowsTemp | select @{n="Temp";e={$_.value | foreach {[int]($_ * 1.8) + 32}}}
+
+# Get low and high temperatures, selecting by index of array
+$LowTempToday = ($TodaysTempF.Temp)[0]
+$LowTempTomorrow = ($tomorrowsTempF.Temp)[0]
+$HighTempToday = ($TodaysTempF.Temp)[-1]
+$HighTempTomorrow = ($tomorrowsTempF.Temp)[-1]
+
+# Filter 7 day forecast for today and tomorrows forecast
+$forecast = $weatherGet | select -ExpandProperty properties | select -ExpandProperty periods | where number -eq 1
+$tomorrowsForecast = ($weatherGet | select -ExpandProperty properties | select -ExpandProperty periods | where startTime -like $tomorrow)[0]
+#endregion
+
+
+Write-Host "Forecast for $((Get-Date).ToShortDateString())" -foregroundcolor Green -backgroundcolor Black
+Write-Host  "Todays Low Temperature: $($LowTempToday)"
+Write-host "Todays High Temperature: $($HighTempToday)"
+
+Write-Host "Detailed Forecast: `n$($forecast.detailedForecast)" -ForegroundColor Green -BackgroundColor Black
+
+write-host "`nTomorrows Low Temperature: $($LowTempTomorrow)"
+write-host "Tomorrows High Temperature: $($HighTempTomorrow)"
+
+Write-Host "`nTomorrows Forecast:`n$($tomorrowsForecast.detailedForecast)" -foregroundcolor Green -BackgroundColor Black
+
+}
+
