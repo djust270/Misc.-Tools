4ec6dd4696503bd9084c02281268e6709c877d7e
David Just <57944742+djust270@users.noreply.github.com>
2022-03-01 09:46:23 -0500

    Create ReRegister-WMIClasses.ps1

---
+++ ReRegister-WMIClasses.ps1
@@ -0,0 +1,15 @@
+<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.195
+	 Created on:   	2/28/2022 12:38 PM
+	 Created by:   	Dave Just
+	 Organization: 	
+	 Filename: ReRegister-WMIClasses     	
+	===========================================================================
+	.DESCRIPTION
+		Re-registers all MOF files. MOF files contain the classes and class instances of the WMI objects contained in the WMI repository
+#>
+
+$mof = gci "$env:Windir\system32\WBEM" -include *.mof, *.mfl -recurse | foreach { $_ | select-string "uninstall" | select -expandproperty path }
+$mof | foreach { & "$env:Windir\system32\WBEM\mofcomp" $_ }
