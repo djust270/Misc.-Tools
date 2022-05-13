714d41dac5fa3620da5151a776c5eb6bd47e868f
unknown <dave.just@gmail.com>
2022-05-13 09:14:28 -0400

    added -f flag

--- GithubBackup.py
+++ GithubBackup.py
@@ -1,8 +1,7 @@
-#This script will clone all github repostirories for a specified user account
+#This script will close all github repostirories for a specified user account
 #Example:
 #GithubBackup.py -s C:\users\Dave\github_repos -u djust270
 import requests
-import json
 import os
 import pathlib
 import argparse
@@ -10,12 +9,16 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", dest="save_location" , type=str, help="Location to save repositories")
 parser.add_argument("-u", dest= "github_user" , type=str, help="Github Username to backup")
+parser.add_argument("-f", action='store_true', dest="force", help="Force operation if target directory is not empty All contents of targer dir will be removed!")
 args = vars(parser.parse_args())
 # List and backup all github repositories
 user = args["github_user"]
 save_location = args["save_location"]
+force = args["force"]
 os.chdir(pathlib.Path(save_location))
-if len(os.listdir(pathlib.Path(save_location))) != 0:
+if len(os.listdir(pathlib.Path(save_location))) != 0 and force !=True:
+    raise Exception('Target directory is not empty. Use the -f flag to force and remove contents of directoy')
+if len(os.listdir(pathlib.Path(save_location))) != 0 and force ==True:
     print("Directory is not empty, removing contents")
     import shutil
     try :
