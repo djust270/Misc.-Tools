effd96865b9b52f37b21386383769a14717b4903
David Just <57944742+djust270@users.noreply.github.com>
2024-08-02 08:31:46 -0400

    Update Get-SystemSpecs.ps1
    Added additional details for model, manufacturer, OS version, and BIOS version. Removed use of gcim alias.

--- Get-SystemSpecs.ps1
+++ Get-SystemSpecs.ps1
@@ -1,26 +1,32 @@
 function Get-SystemSpecs {
-$processorSpecs = gcim win32_processor
+$processorSpecs = Get-CimInstance win32_processor
 $processorName = $processorSpecs.Name
 $processorSpeed = [string]([math]::round(($processorSpecs.CurrentClockSpeed /1000),2)) + 'ghz'
 $processorCores = $processorSpecs.NumberOfCores
 $processorThreads = $processorSpecs.ThreadCount
-$ramTotal = "{0:N2}" -f (((gcim CIM_PhysicalMemory | select -ExpandProperty Capacity) | measure -Sum).sum /1gb ) 
-
+$ramTotal = "{0:N2}" -f (((Get-CimInstance CIM_PhysicalMemory | select -ExpandProperty Capacity) | measure -Sum).sum /1gb ) 
+$system = Get-CimInstance Win32_ComputerSystem
+$bios = Get-CimInstance Win32_Bios
+$os = Get-CimInstance win32_operatingsystem
 function HDD-Details {
-$hdd = gcim Win32_DiskDrive | where {$_.MediaType -like "Fixed*"}
+$hdd = Get-CimInstance Win32_DiskDrive | where {$_.MediaType -like "Fixed*"}
 $hdd | ForEach{$_.caption + ", Capacity: " + [math]::round(($_.Size / 1GB),'2') + "GB"  }
 }
 
 $Specs = [pscustomobject]@{
+'Manufacturer' = $system.manufacturer
+'Model' = $system.model
+'SerialNumber' = $bios.SerialNumber
+'BIOS_Version' = $bios.SMBIOSBIOSVersion
 'Processor' = $processorName
 'Cores' = $processorCores
 'ThreadCount' = $processorThreads
 'ProcessorClockSpeed' = $processorSpeed
 'Physical Memory Size' = $ramTotal + ' GB'
-'System Type' = gcim win32_operatingsystem | select -ExpandProperty OSArchitecture
+'System Type' = $os.OSArchitecture
 'Hard Drive(s)' = HDD-Details
-'Serial' = gcim win32_bios | select -expandproperty serialnumber
-'OS' = gcim win32_operatingsystem | select -expandproperty caption
+'OS' = $os.caption
+'OS_Version' = $os.Version
 }
 return $Specs
 }
